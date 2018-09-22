2859a309cf3e3e972ae6a29d9fd0be193c6696fc
ronron <ant@n.io>
2018-09-22 19:34:10 +0200

    .

--- manifests/zones.pp
+++ manifests/zones.pp
@@ -1,12 +1,12 @@
 class knot::zones (
 
-	$nodename = hiera('nodename')
-	$service_name   = hiera('service_name'),
-  $knot_zones_directory = hiera('knot_zones_directory')
+	$nodename = hiera('nodename'),
+	$service_name   = hiera('knot::service::service_name'),
+	$knot_zones_directory = hiera('knot_zones_directory'),
         
 	) {
  
-	file { $knot_zones_directory":
+	file { $knot_zones_directory:
 		ensure => present,
 		sourceselect => all,
 		source => ["puppet:///modules/knot/${nodename}/var/lib/knot","/var/lib/knot "],
