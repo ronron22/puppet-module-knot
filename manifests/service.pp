914b0da5a0608003c18388ce1bef8e797000980d
ronron <ant@n.io>
2018-09-22 19:10:34 +0200

    .

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,9 +11,9 @@ class knot::service (
 	}
   if $service_manage == true {
     service { $service_name:
-			ensure  => $service_ensure,
-			enable  => $service_enable,
-      provider => provider,
+		  ensure  => $service_ensure,
+      enable  => $service_enable,
+		  provider => systemd,
       #hasstatus  => true,
       #hasrestart => true,
     }
